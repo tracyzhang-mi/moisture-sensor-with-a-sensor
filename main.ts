bcb16b8ae466ddb8799ef8504963f10a7a38d66a
Tracy Zhang <73887948+tracyzhang-mi@users.noreply.github.com>
2022-06-15 15:31:32 -0400

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,12 +1,40 @@
-let MoistureReading = 0
-basic.forever(function () {
-    MoistureReading = Math.map(pins.analogReadPin(AnalogPin.P0), 0, 750, 0, 100)
-    led.plotBarGraph(
-    MoistureReading,
-    100
-    )
-    basic.pause(500)
+input.onButtonPressed(Button.A, function () {
+    MoistureReading = Math.map(pins.analogReadPin(AnalogPin.P0), 0, CalibrateWet, 0, 100)
     basic.showNumber(Math.round(MoistureReading))
-    basic.pause(500)
     basic.clearScreen()
+})
+input.onButtonPressed(Button.B, function () {
+    CalibrateWet = pins.analogReadPin(AnalogPin.P0)
+    basic.showIcon(IconNames.Yes)
+})
+let MoistureReading = 0
+let CalibrateWet = 0
+CalibrateWet = 750
+basic.forever(function () {
+    MoistureReading = Math.map(pins.analogReadPin(AnalogPin.P0), 0, CalibrateWet, 0, 3)
+    if (MoistureReading <= 1) {
+        basic.showLeds(`
+            # . . . #
+            # # . # #
+            . # . # .
+            . # . # .
+            . # # # .
+            `)
+    } else if (MoistureReading <= 1 && MoistureReading <= 2) {
+        basic.showLeds(`
+            # . . . #
+            # # . # #
+            . # # # .
+            . # # # .
+            . # # # .
+            `)
+    } else {
+        basic.showLeds(`
+            # # # # #
+            # # # # #
+            . # # # .
+            . # # # .
+            . # # # .
+            `)
+    }
 })
