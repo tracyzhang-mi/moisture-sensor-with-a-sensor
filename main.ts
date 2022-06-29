e68ea7571581109e0903cfe6d03468d0b7ab875f
Tracy Zhang <73887948+tracyzhang-mi@users.noreply.github.com>
2022-06-29 16:14:50 -0400

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,40 +1,21 @@
-input.onButtonPressed(Button.A, function () {
-    MoistureReading = Math.map(pins.analogReadPin(AnalogPin.P0), 0, CalibrateWet, 0, 100)
-    basic.showNumber(Math.round(MoistureReading))
-    basic.clearScreen()
-})
-input.onButtonPressed(Button.B, function () {
-    CalibrateWet = pins.analogReadPin(AnalogPin.P0)
-    basic.showIcon(IconNames.Yes)
-})
-let MoistureReading = 0
-let CalibrateWet = 0
-CalibrateWet = 750
 basic.forever(function () {
-    MoistureReading = Math.map(pins.analogReadPin(AnalogPin.P0), 0, CalibrateWet, 0, 3)
-    if (MoistureReading <= 1) {
-        basic.showLeds(`
-            # . . . #
-            # # . # #
-            . # . # .
-            . # . # .
-            . # # # .
-            `)
-    } else if (MoistureReading <= 1 && MoistureReading <= 2) {
-        basic.showLeds(`
-            # . . . #
-            # # . # #
-            . # # # .
-            . # # # .
-            . # # # .
-            `)
-    } else {
-        basic.showLeds(`
-            # # # # #
-            # # # # #
-            . # # # .
-            . # # # .
-            . # # # .
-            `)
-    }
+    basic.showIcon(IconNames.Happy)
+    dht11_dht22.queryData(
+    DHTtype.DHT11,
+    DigitalPin.P1,
+    true,
+    false,
+    true
+    )
+    basic.showNumber(dht11_dht22.readData(dataType.humidity))
+    basic.showIcon(IconNames.Heart)
+    basic.showNumber(dht11_dht22.readData(dataType.temperature))
+    datalogger.log(
+    datalogger.createCV("H", dht11_dht22.readData(dataType.humidity)),
+    datalogger.createCV("T", dht11_dht22.readData(dataType.temperature)),
+    datalogger.createCV("t", input.temperature())
+    )
+    basic.showIcon(IconNames.SmallHeart)
+    basic.showNumber(input.temperature())
+    basic.pause(1000)
 })
